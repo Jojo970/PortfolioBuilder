cccbeff837c027c3877ab9ae2cb6534d93e93744
Jong <jjleejong@gmail.com>
2023-08-16 15:29:03 -0500

    starting over

--- route/user.route.ts
+++
@@ -1,8 +0,0 @@
-const userController = require('../controller/user.controller');
-
-module.exports = (app) => {
-  app.post('/register', userController.register);
-  app.post('/login', userController.login);
-  app.post('/logout', userController.logout);
-  app.get('/api/current-user', userController.getLoggedInUser);
-};
